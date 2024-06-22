1dfcd3405e42cd1a44f590e0dc0f2281bb49fdf2
Filip Wądołowski <84192488+fkwadolowski@users.noreply.github.com>
2024-06-23 01:09:48 +0200

    Rendering html elements

--- hello.py
+++ hello.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World"
+    return ('<h1 style="text-align: center"> World!</h1><p> This is ' 
+            'paragraph.</p><img '
+            'src="https://media.giphy.com/media/v1'
+            '.Y2lkPTc5MGI3NjExb2dteWZocGhjanIwYWpwcWM2bHliaWNhamZodGtzOGNmMGh2Z3FlNyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/3oEdvaAhIxot8KIKiI/giphy.gif" width=200>')
 
 
 # aby zadziałało set FLASK_APP=hello.py
@@ -19,7 +22,7 @@ def bye():
 
 
 @app.route("/username/<name>/<int:number>")  # Variables and path converting
-def greet(name,number):
+def greet(name, number):
     return f"Hello {name},do you like number:{number}?"
 
 
